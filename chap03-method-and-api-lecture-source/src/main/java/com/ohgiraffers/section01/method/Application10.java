bca168bddfcab4f641ecabda0fd7837eadb608d6
weed97 <weed9935@gmail.com>
2024-05-16 14:57:58 +0900

    메서드 패키지

---
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application10.java
@@ -0,0 +1,46 @@
+package com.ohgiraffers.section01.method;
+
+public class Application10 {
+    public static void main(String[] args) {
+
+
+        int num1 = 16;
+        int num2 = 5;
+        Application10 app10 = new Application10();
+        System.out.println("두 수를 더한 결과 : " + app10.plusNum(num1, num2));
+        System.out.println("두 수를 뺀 결과 : " + app10. (num1, num2));
+        System.out.println("두 수를 곱한 결과 : " + app10.(num1, num2));
+        System.out.println("두 수를 나눈 결과 : " + app10. (num1, num2));
+        System.out.println("두 수를 나머지 한 결과 : " + app10. (num1, num2));
+
+        // 더하기
+
+
+
+
+
+        }
+
+        // 빼기
+
+
+        }
+
+        // 곱하기
+
+        }
+
+        // 나눗셈
+
+        }
+
+        // 나머지
+
+
+        }
+
+
+  }
+
+
+    }
