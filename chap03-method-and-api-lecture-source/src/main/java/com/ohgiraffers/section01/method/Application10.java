8069ff0302ae11513242fe7ed60aaaccba46970b
weed97 <weed9935@gmail.com>
2024-05-16 16:16:51 +0900

    연습문제 논스테틱

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application10.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section01/method/Application10.java
@@ -7,40 +7,43 @@ public class Application10 {
         int num1 = 16;
         int num2 = 5;
         Application10 app10 = new Application10();
-        System.out.println("두 수를 더한 결과 : " + app10.plusNum(num1, num2));
-        System.out.println("두 수를 뺀 결과 : " + app10. (num1, num2));
-        System.out.println("두 수를 곱한 결과 : " + app10.(num1, num2));
-        System.out.println("두 수를 나눈 결과 : " + app10. (num1, num2));
-        System.out.println("두 수를 나머지 한 결과 : " + app10. (num1, num2));
-
-        // 더하기
-
-
-
-
-
-        }
-
-        // 빼기
-
-
-        }
-
-        // 곱하기
-
-        }
-
-        // 나눗셈
-
-        }
-
-        // 나머지
-
-
-        }
-
-
-  }
-
-
+        System.out.println("두 수의 덧셈 : " + app10.addNum(num1, num2));
+        System.out.println("두 수의 뺄셈 : " + app10.addMinus(num1, num2));
+        System.out.println("두 수의 곱셈 : " + app10.addMultiple(num1, num2));
+        System.out.println("두 수의 나눗셈 : " + app10.addDivide(num1, num2));
+        System.out.println("두 수의 나머지 : " + app10.addDivision(num1, num2));
     }
+
+    public int addNum(int num1, int num2) {
+
+        return num1 + num2;
+    }
+
+    // 빼기
+    public int addMinus(int num1, int num2) {
+
+        return num1 - num2;
+    }
+
+
+    // 곱하기
+    public int addMultiple(int num1, int num2) {
+
+        return num1 * num2;
+    }
+
+
+    // 나눗셈
+    public int addDivide(int num1, int num2) {
+
+        return num1 / num2;
+    }
+
+
+    // 나머지
+    public int addDivision(int num1, int num2) {
+
+        return num1 % num2;
+    }
+
+}
