a3fb3475dd1b70a232a9ba1ade06c82172e247ca
weed97 <weed9935@gmail.com>
2024-05-17 18:10:25 +0900

    스캐너

--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section04/scanner/Application1.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section04/scanner/Application1.java
@@ -34,47 +34,49 @@ public class Application1 {
         System.out.println("나이를 입력하세요");
         int age = sc.nextInt(4); // 매개변수 : 진법
         System.out.println("입력하신 나이는" + age + " 입니다.");
-//
-//        // nestByte(), nextShort()
-//
-//
-//        /*
-//        * 정수형(long)
-//        *
-//        * nextLong() : 입력받은 값을 long형으로 반환해준다.
-//        * */
-//        System.out.println("원하는 월급을 입력해주세요 : ");
-//        long money = sc.nextLong();
-//        System.out.println("입력하신 금액은 " + money + " 입니다.");
-//
-//        /*
-//        * 실수형(float)
-//        *
-//        * nextFloat() : 입력받은 값을 float형으로 반환해준다.
-//        * */
-//        System.out.println("키를 입력해주세요 :");
-//        float height = sc.nextFloat();
-//        System.out.println("입력하신 키는 " + height + "cm 입니다.");
-//
-//        /*
-//        * 실수형(double)
-//        *
-//        * nextDouble() : 입력받은 값을 double형으로 반환해준다.*/
-//
-//        System.out.println("원주율(파이)를 아는 만큼 입력하세요");
-//        double pi = sc.nextDouble();
-//        System.out.println("입력한 원주율은 " + pi + "입니다.");
-//
-//        /*
-//        * 논리형(boolean)
-//        *
-//        * nextBoolean() : 입력받은 값을 boolean형으로 반환한다.
-//        * */
-//        System.out.println("참과 거짓중에 한가지를 true or false 로 입력해주세요");
-//        System.out.println("나는 복습을 잘하고 있다.");
-//        boolean isTrue = sc.nextBoolean();
-//        System.out.println("입력하신 논리 값은 " + isTrue + "입니다.");
 
+        // nestByte(), nextShort()
+
+
+        /*
+        * 정수형(long)
+        *
+        * nextLong() : 입력받은 값을 long형으로 반환해준다.
+        * */
+        System.out.println("원하는 월급을 입력해주세요 : ");
+        long money = sc.nextLong();
+        System.out.println("입력하신 금액은 " + money + " 입니다.");
+
+        /*
+        * 실수형(float)
+        *
+        * nextFloat() : 입력받은 값을 float형으로 반환해준다.
+        * */
+        System.out.println("키를 입력해주세요 :");
+        float height = sc.nextFloat();
+        System.out.println("입력하신 키는 " + height + "cm 입니다.");
+
+        /*
+        * 실수형(double)
+        *
+        * nextDouble() : 입력받은 값을 double형으로 반환해준다.*/
+
+        System.out.println("원주율(파이)를 아는 만큼 입력하세요");
+        double pi = sc.nextDouble();
+        System.out.println("입력한 원주율은 " + pi + "입니다.");
+
+        /*
+        * 논리형(boolean)
+        *
+        * nextBoolean() : 입력받은 값을 boolean형으로 반환한다.
+        * */
+        System.out.println("참과 거짓중에 한가지를 true or false 로 입력해주세요");
+        System.out.println("나는 복습을 잘하고 있다.");
+        boolean isTrue = sc.nextBoolean();
+        System.out.println("입력하신 논리 값은 " + isTrue + "입니다.");
+
+        // 에러 발생
+        sc.nextLine();
         System.out.println("아무 문자나 입력하세요");
         String str = sc.nextLine();
         char ch = str.charAt(0);
--- chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section04/scanner/Application2.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/ohgiraffers/section04/scanner/Application2.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.section04.scanner;
 
+import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.DoubleStream;
 
 public class Application2 {
     public static void main(String[] args) {
@@ -10,13 +12,22 @@ public class Application2 {
          * next() : 공백문자나 개행문자 전 까지 읽어서 문자열로 반환한다.
          * */
         Scanner sc = new Scanner(System.in);
-        System.out.println("인사말을 입력해주세요");
-        String greeting = sc.nextLine(); // 안녕하세요 반갑습니다 -> 전부 저장
-        System.out.println(greeting);
+//        System.out.println("인사말을 입력해주세요");
+//        String greeting = sc.nextLine(); // 안녕하세요 반갑습니다 -> 전부 저장
+//        System.out.println(greeting);
+//
+//        System.out.println("인사말을 입력해주세요");
+//        String greeting2 = sc.next(); // 안녕하세요 --> 이거만 저장
+//        System.out.println(greeting2);
+//
+        int a = sc.nextInt();
+        Random random = new Random();
 
-        System.out.println("인사말을 입력해주세요");
-        String greeting2 = sc.next(); // 안녕하세요 --> 이거만 저장
-        System.out.println(greeting2);
+        int randInt = random.nextInt(10,30);
+        System.out.println(randInt);
+
+
+
 
 
 
