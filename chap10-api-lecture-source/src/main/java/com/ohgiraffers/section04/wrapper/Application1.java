e1586bf80daaec5aac039e193704e7a5c10dfa0e
weed97 <weed9935@gmail.com>
2024-05-29 13:15:28 +0900

    래퍼클래스 기본타입

---
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/section04/wrapper/Application1.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.section04.wrapper;
+
+public class Application1 {
+    public static void main(String[] args) {
+
+        /*
+         * 기본타입          레퍼클래스
+         * byte              Byte
+         * short             Short
+         * int               Int
+         * long              Long
+         * float             Float
+         * double            Double
+         * char              Char
+         * boolean           Boolean
+         * */
+
+        /*
+        * 박싱(Boxing)과 언박싱(UnBoxing)
+        * 기본타입에서 레퍼클래스의 인스턴스로 인스턴스화하는 것을 박싱(Boxing)
+        * 레퍼클래스 타입의 인스턴스 기본 타입으로 변경하는 것을 언박싱(UnBoxing)
+        * */
+
+    }
+}
